3ca939fb86cb8964a769736b9620fc2ec47603ff
Arthur <arthursh@outlook.com.br>
2024-05-26 17:58:27 -0300

    v11

--- CursoEmVideo/aula7a.py
+++ CursoEmVideo/aula7a.py
@@ -1,2 +1,4 @@
-nome = input('DIGITE SEU NOME: ')
-print('='*20, nome, '='*20)
+n1 = int(input('Um valor: '))
+n2 = int(input('Outro valor: '))
+print('a soma é {}\no produto é {}\ndivisão é {}'.format(n1+n2, n1*n2, n1/n2))
+print('divisão inteira {}\npotencia {}'.format(n1//n2, n1**n2))
