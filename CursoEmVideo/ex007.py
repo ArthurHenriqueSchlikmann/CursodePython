c2e9019e1eed119badb871b45f3451d4277fe4f9
ArthurHenriqueSchlikmann <arthursh@outlook.com.br>
2026-02-25 18:14:57 -0300

    volta as aulas

---
+++ CursoEmVideo/ex007.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+nota1 = float(input('Primeira nota do aluno: '))
+nota2 = float(input('Segunda nota do aluno: '))
+media = (nota1 + nota2) / 2
+print('A média final entre ', nota1, ' e ', nota2, ' é ', media)
